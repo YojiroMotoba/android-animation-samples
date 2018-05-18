8346c37bd020e4981df2c19cfd8634ef508f3f01
YojiroMotoba <ymotoba@gmail.com>
2018-05-18 17:39:58 +0900

    アクティイティ一覧アクティビティの追加

--- app/src/main/java/com/example/animationsample/ActivityListActivity.kt
+++ app/src/main/java/com/example/animationsample/ActivityListActivity.kt
@@ -1,2 +1,31 @@
 package com.example.animationsample
 
+import android.content.ComponentName
+import android.content.Intent
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_list_activity.*
+
+
+class ActivityListActivity : AppCompatActivity() {
+
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_activity)
+        val packageInfo: PackageInfo = packageManager.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES)
+        val activityNames = mutableListOf<String>()
+        packageInfo.activities.forEach { activityInfo ->
+            activityNames.add(activityInfo.name)
+        }
+        list.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, activityNames)
+        list.setOnItemClickListener { _, _, position, _ ->
+            startActivity(Intent().also {
+                it.component = ComponentName(packageName, packageInfo.activities[position].name)
+            })
+        }
+    }
+}
